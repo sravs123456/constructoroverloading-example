1df31df205e56bff71e4ce30e8ded2add118c162
sravya <chennamsettisravya2002@gmail.com>
2024-02-11 13:12:36 +0530

    hello1

--- src/Address2.java
+++ src/Address2.java
@@ -1,8 +1,9 @@
 public class Address2{
     public int a;
     public int b;
-    public int add(int a,int b){
-        if(a && b){
+    public int c;
+    public int add(int a,int b int c){
+        if(a && b && c){
             int c=a+b
             System.out.println(c);
 
---
+++ src/Address3.java
@@ -0,0 +1,13 @@
+public class Address2 {
+    public int a;
+    public int b;
+    public int c;
+
+    public int add(int a, int b int c) {
+        if (a && b && c) {
+            int c = a + b
+            System.out.println(c);
+
+        }
+    }
+}
