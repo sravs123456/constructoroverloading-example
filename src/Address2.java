781eff4a559542dc08ecde8e4cc0c781132ad480
sravya <chennamsettisravya2002@gmail.com>
2024-02-11 12:51:20 +0530

    hello1

---
+++ src/Address2.java
@@ -0,0 +1,13 @@
+public class Address2{
+    public int a;
+    public int b;
+    public int add(int a,int b){
+        if(a && b){
+            int c=a+b
+            System.out.println(c);
+
+        }
+    }
+
+
+}
